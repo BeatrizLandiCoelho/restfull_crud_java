876c4f851d9e8aa1a69e72c9cc1028ec2e7c1dc2
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2024-05-05 21:57:08 -0300

    user  crud finished

--- src/main/java/com/example/demoz/Controllers/UserController.java
+++ src/main/java/com/example/demoz/Controllers/UserController.java
@@ -1,9 +1,17 @@
 package com.example.demoz.Controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demoz.Models.User;
@@ -20,7 +28,56 @@ public class UserController {
     }
     
     @GetMapping("/allusers")
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    public ResponseEntity <List<User>> getAllUsers(){
+        List<User> users =  userRepository.findAll();
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        }
+
     }
-}
+
+    //crete user
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        try {
+            User newUser = userRepository.save(user);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    //update user
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User userData) {
+        User existingUser = userRepository.findById(id).orElse(null);
+
+        if (existingUser != null) {
+            existingUser.setName(userData.getName());
+            existingUser.setEmail(userData.getEmail());
+            existingUser.setPassword(userData.getPassword());
+
+            User updatedUser = userRepository.save(existingUser);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    //delete user
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id) {
+        try {
+            userRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+} 
+  
